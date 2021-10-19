ad31bd835ecf753a4b9d663c0c0914420f1f7cd0
homeboy445 <akshitsan13@gmail.com>
2021-10-19 20:33:59 +0530

    Fixed the re-including of the headers

--- extension.js
+++ extension.js
@@ -1,29 +1,15 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 const vscode = require("vscode");
 const { keywords } = require("./keywords");
 const { RemoveUnusedHeaders } = require("./Utility");
-
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
 
 /**
  * @param {vscode.ExtensionContext} context
  */
 function activate(context) {
-  // Use the console to output diagnostic information (console.log) and errors
-  // (console.error) This line of code will only be executed once when your
-  // extension is activated
-  console.log('Congratulations, your extension "autoinclude" is now active!');
-
-  // The command has been defined in the package.json file
-  // Now provide the implementation of the command with  registerCommand
-  // The commandId parameter must match the command field in package.json
+  console.log("ACTIVE");
   let disposable = vscode.commands.registerCommand(
     "autoinclude.autoInclude",
     function () {
-      // The code you place here will be executed every time your command is
-      // executed
       const editor = vscode.window.activeTextEditor;
       vscode.commands.executeCommand("editor.action.selectAll").then(() => {
         vscode.window.showInformationMessage("Including required libraries...");
@@ -40,7 +26,7 @@ function activate(context) {
             if (text[i] === "\n") {
               words.push("@");
             }
-            if (text[i] === " "){
+            if (text[i] === " ") {
               words.push("@$%");
             }
             s = "";
@@ -59,17 +45,19 @@ function activate(context) {
         }
         let str = "";
         [...headers].map((item) => {
-          if (words.find((item1) => item1 === `<${item}>`)) {
-            return;
+          let flag = true;
+          words.map((item1) => {
+            flag &= item1.lastIndexOf(item) === -1;
+          });
+          if (flag) {
+            str += `#include <${item}>\n`;
           }
-          str += `#include <${item}>\n`;
         });
         editor.edit((editBuilder) =>
           editBuilder.replace(editor.selection, str + text)
         );
         vscode.commands.executeCommand("cursorMove", { to: "viewPortTop" });
       });
-      // Display a message box to the user
       vscode.window.showInformationMessage("Done!");
     }
   );
@@ -77,7 +65,6 @@ function activate(context) {
   context.subscriptions.push(disposable);
 }
 
-// this method is called when your extension is deactivated
 function deactivate() {}
 
 module.exports = {
